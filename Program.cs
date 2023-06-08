ce9749141dc2cebba7389e687d219ae6e12713df
Vladimir <vermakov410@gmail.com>
2023-06-09 02:15:43 +0300

    Задача 3(23)

--- Program.cs
+++ Program.cs
@@ -40,23 +40,40 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Введите координату x1: ");
-double x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату y1: ");
-double y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату z1: ");
-double z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату x2: ");
-double x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату y2: ");
-double y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату z2: ");
-double z2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату x1: ");
+// double x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y1: ");
+// double y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z1: ");
+// double z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату x2: ");
+// double x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y2: ");
+// double y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z2: ");
+// double z2 = Convert.ToInt32(Console.ReadLine());
 
-double Lenght(double X1, double Y1, double Z1, double X2, double Y2, double Z2)
+// double Lenght(double X1, double Y1, double Z1, double X2, double Y2, double Z2)
+// {
+//     return Math.Round(Math.Sqrt(Math.Pow((X1 - X2),2) + Math.Pow((Y1 - Y2),2) + Math.Pow((Z1 - Z2),2)), 2);
+// // double d =Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2));
+// }
+// Console.Write("Расстояние между заданными точками: ");
+// Console.Write(Lenght(x1, y1, z1, x2, y2, z2));
+
+
+﻿//Задача 23
+//Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу 
+// кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= num)
 {
-    return Math.Round(Math.Sqrt(Math.Pow((X1 - X2),2) + Math.Pow((Y1 - Y2),2) + Math.Pow((Z1 - Z2),2)), 2);
-// double d =Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2));
+    Console.WriteLine(Math.Pow(count, 3));
+    count++;
 }
-Console.Write("Расстояние между заданными точками: ");
-Console.Write(Lenght(x1, y1, z1, x2, y2, z2));
