29a307654cf85d081bdc5eb0c6a8081b1c401db1
Vladimir <vermakov410@gmail.com>
2023-06-09 01:37:16 +0300

    Задача 1(19)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if((num < 10000) || (num > 99999))
+{
+    Console.WriteLine("Неправильное число");
+}
+
+int a1 = num % 10;
+int b1 = num / 10;
+int a2 = b1 % 10;
+int b2 = b1 / 10;
+int a3 = b2 % 10;
+int b3 = b2 / 10;
+int a4 = b3 % 10;
+int a5 = b3 /10;
+
+if((a1 == a5) && (a2 == a4))
+{
+    Console.WriteLine("Это число палиндром");
+}
+else
+{
+    Console.WriteLine("Это число НЕ палиндром");
+}
